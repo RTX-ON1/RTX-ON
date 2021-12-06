fbf5897d12f858903a4407900750a594edfc86fa
XenonJ <17609005839@163.com>
2021-12-06 21:56:32 +0800

    game

--- game/Chinese College Students/Assets/Scripts/MainMenu.cs
+++ game/Chinese College Students/Assets/Scripts/MainMenu.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject LoadGamePanel;
+    public GameObject SettingsPanel;
+
     // Start is called before the first frame update
-   public void PlayGame()  {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-    } 
-    public void GoToSettingsMenu(){
-        SceneManager.LoadScene("SettingsMenu");
-   }
-    
-    public void  GoToMainMenu(){
-        SceneManager.LoadScene("MainMenu");
+
+    public void Play()
+    {
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
+            LoadGamePanel.SetActive(true);
+        else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void ExitGame(){
+    public void PlayGame()
+    {
+        SaveGame savegame = new SaveGame();
+        savegame.LoadGameFunc();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToSettingsMenu()
+    {
+
+    }
+    
+    public void GoToMainMenu()
+    {
+
+    }
+
+    public void ExitGame()
+    {
         Application.Quit();
     }
+
+    public void onclickReturn1()
+    {
+        LoadGamePanel.SetActive(false);
+    }
+
 }

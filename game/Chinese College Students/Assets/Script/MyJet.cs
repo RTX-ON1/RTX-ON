a9b31719871b9e78250f7ebe4752619e9cb5282a
蔡晨 <151370772@qq.com>
2021-12-09 17:21:14 +0800

    update plane fight

--- game/Chinese College Students/Assets/Script/MyJet.cs
+++ game/Chinese College Students/Assets/Script/MyJet.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
 public class MyJet : MonoBehaviour
@@ -11,6 +10,7 @@ public class MyJet : MonoBehaviour
     private float interval = 0.4f;
     private float count = 0;
     public static bool isdestory;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,15 +32,21 @@ public class MyJet : MonoBehaviour
 
         // 按键响应
         float step = moveSpeed * Time.deltaTime;
-        
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            if(transform.position.x<-2){
+            return;
+        }
             transform.Translate(-step, 0, 0); 
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            if(transform.position.x>2){
+            return;
+        }
             transform.Translate(step, 0, 0);
         }
+
     }
 
     private void Fire()
